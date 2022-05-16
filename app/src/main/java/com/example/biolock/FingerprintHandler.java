2c7efb04631cdf18f10a50e7dccebf95b1e36ba3
Kiran J <56204382+KiranJoji@users.noreply.github.com>
2022-05-16 14:58:33 -0700

    Update FingerprintHandler.java

--- app/src/main/java/com/example/biolock/FingerprintHandler.java
+++ app/src/main/java/com/example/biolock/FingerprintHandler.java
@@ -11,6 +11,7 @@ import android.os.CancellationSignal;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+//This class will help us in writting the various methods we will be using to configure actions for the program to do with biometric data gathered by the user's finger
 @TargetApi(Build.VERSION_CODES.M)
 public class FingerprintHandler extends FingerprintManager.AuthenticationCallback {
 
@@ -21,31 +22,32 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
         this.context = context;
     }
 
-    //Starts authentication of the fingerprint
+    //This method is used for conducting an authentication of the fingerprints on the phone. CryptoObject helps the system identify
+    //whether there has been a new fingerprint added to the device since the last time the app was run.
     public void startAutha(FingerprintManager fingerprintManager, FingerprintManager.CryptoObject cryptoObject) {
         cancellationSignal = new CancellationSignal();
         fingerprintManager.authenticate(cryptoObject, null, 0, this, null);
     }
 
-    //On error or failure, sends the error to the user
+    //method to show to user for determining whether there has been an error within the fingerprint handler package running the processes regarding the fingerprint
     @Override
     public void onAuthenticationError(int errorCode, CharSequence errString) {
         this.update("There was an Auth Error. " + errString, false);
     }
 
-    //On error or failure, sends the error to the user
+    //method to show the user if the authentication failed or is not correct
     @Override
     public void onAuthenticationFailed() {
         this.update("Auth Failed. ", false);
     }
 
-    //On error or failure, sends the error to the user
+    //method regarding external error messages. Will display error help string to help the user fix the error
     @Override
     public void onAuthenticationHelp(int helpCode, CharSequence helpString) {
         this.update("Error: " + helpString, false);
     }
 
-    //On success, sends the user into the next page
+    //method to move to the app page when the fingerprint authentication successfully runs through and there has been a match with the correct fingerprint
     @Override
     public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
         this.update("You can now access the app.", true);
@@ -54,7 +56,7 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
         context.startActivity(letsgo);
     }
 
-    //On success, changing image to checkmark and changing text to reveal success
+    //method used for updated objects on the screen to output the results of the processes ran above
     private void update(String s, boolean b) {
         cancellationSignal.cancel();
         TextView paraLabel = (TextView) ((Activity) context).findViewById(R.id.paraLabel);
